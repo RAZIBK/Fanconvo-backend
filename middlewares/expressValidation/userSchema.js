b2bfa8cced24e00bbafbb39dd06f07bd7722db57
Razi bk <razibeppukaran@gmail.com.com>
2023-01-23 21:06:26 +0530

    Swagger connected

---
+++ middlewares/expressValidation/userSchema.js
@@ -0,0 +1,12 @@
+const { body } = require("express-validator");
+
+const registerSchema = [
+  body("firstName").exists().withMessage('First Name is required'),
+  body("lastName").exists().withMessage('Last Name is required'),
+  body("userName").exists().withMessage('User Name is required'),
+  body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 chars long'),
+  body("email").isEmail('Email must contain a valid email address'),
+  body("timeZone").exists().withMessage('TimeZone is required'),
+];
+
+module.exports = {registerSchema};
--- routes/userRouter/userRouter.js
+++ routes/userRouter/userRouter.js
@@ -1,10 +1,14 @@
 const express=require('express')
 const { userSignupCtrl } = require('../../controller/user/userControllers')
+const { validateRequestSchema } = require('../../middlewares/expressValidation/expressValidation')
+const { registerSchema } = require('../../middlewares/expressValidation/userSchema')
 
 const userRouter=express.Router()
 
 
-userRouter.post('/',userSignupCtrl)
+
+
+userRouter.post('/',registerSchema,validateRequestSchema,userSignupCtrl)
 
 
 module.exports=userRouter
